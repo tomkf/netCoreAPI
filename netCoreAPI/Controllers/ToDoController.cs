341f08d10ccd8cbed0bd4d049a5379f819e093a5
tomkf <thomaskeithferris@gmail.com>
2020-01-26 20:12:28 -0800

    api controller with methods

---
+++ netCoreAPI/Controllers/ToDoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using netCoreAPI.Services;
+
+namespace netCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public ToDoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        //when todo hit, this method fired
+        [HttpGet]
+        public IEnumerable<ToDo> GetAll()
+        {
+            return _context.ToDos.ToList();
+        }
+
+        [HttpGet("id", Name = "GetTodo")]
+        {
+            public IActionResult GetbyId(long id)
+        {
+            return new ObjectResult();
+            {
+                var item = _context.ToDos.FirstOrDefault(t => t.Id == id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                return new ObjectResult(item);
+            }
+        }
+        }
+    }
+}
