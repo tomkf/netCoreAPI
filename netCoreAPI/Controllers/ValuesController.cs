630bb58b24eb227eb05bac710f19e9fcc57a22de
tomkf <thomaskeithferris@gmail.com>
2020-01-27 10:34:42 -0800

    update http methods

--- netCoreAPI/Controllers/ValuesController.cs
+++ netCoreAPI/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using netCoreAPI.Services;
 
 namespace netCoreAPI.Controllers
 {
@@ -26,20 +27,44 @@ namespace netCoreAPI.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Create([FromBody]ToDo todo)
         {
+            if (todo.Description == null || todo.Description == "")
+            {
+                return BadRequest();
+            }
+            _context.ToDos.Add(todo);
+            _context.SaveChanges();
+            return new ObjectResult(todo);
         }
+
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
         {
+            var item = _context.ToDos.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
         }
 
+
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete]
+        [Route("MyDelete")] // Custom route
+        public IActionResult MyDelete(long Id)
         {
+            var item = _context.ToDos.Where(t => t.Id == Id).FirstOrDefault();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            _context.ToDos.Remove(item);
+            _context.SaveChanges();
+            return new ObjectResult(item);
         }
     }
 }
