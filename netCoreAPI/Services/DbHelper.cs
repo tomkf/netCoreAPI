4c26d8dd0c73fa1b910428bbdd2ff71178de3966
tomkf <thomaskeithferris@gmail.com>
2020-01-26 19:40:20 -0800

    added sqlite helper

---
+++ netCoreAPI/Services/DbHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace netCoreAPI.Services
+{
+    public class ToDo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<ToDo> ToDos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDo>().HasData(
+                new { Id = 1, Description = "Clean house", IsComplete = false },
+                new { Id = 2, Description = "Bake cake", IsComplete = false }
+            );
+        }
+    }
+}
